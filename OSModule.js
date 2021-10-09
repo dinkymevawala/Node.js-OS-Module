95c7dd3772dbaa868b5496caac3e9b40c06e0fbf
unknown <dinkymevawala.dcs22n@vnsgu.ac.in>
2021-10-09 18:26:32 +0530

    Final Submit

--- OSModule.js
+++ OSModule.js
@@ -1,3 +1,6 @@
 var os = require('os');
 console.log("OS CPU architecture: " + os.arch());
 console.log("Free memory of the system: " + Math.round(((os.freemem() / 1024 / 1024 / 1024) * 100)) / 100 + " GB");
+console.log("Total memory of the system: " + Math.round(((os.totalmem() / 1024 / 1024 / 1024) * 100)) / 100 + " GB");
+console.log("OS Platform: " + os.platform());
+console.log("Current User: " + os.userInfo().username);
